689f46ec3eda821d34fb322342a4b28b2c4f561f
Nicolas barbosa <nico.mb2003@gmail.com>
2023-09-22 09:22:46 -0300

    Atualizações das classes

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
 import random
-
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response, request
+from newcadastro import Cadastro
 
 app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
 
-curriculos = [
-    {
-        "id": random.random(),
-        "responsavel": "Pedro"
-    },
-]
-
-
-@app.route('/curriculos')
+@app.route('/lista_cadastrados', methods=['GET'])
 def index():
-    return jsonify(curriculos)
+    return make_response (
+        jsonify(
+            mensagem='Lista de cadastros.',
+            dados=Cadastro
+        )
+    )
+
+@app.route('/cadastro', methods=['POST'])
+def create_cadastro():
+    cadastro = request.json
+    return "200ok"
 
 
 app.run(port=8000, host='localhost', debug=True)
