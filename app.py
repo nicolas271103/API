5b70106195828cf3e37594b6556c6d9c44da4e81
Nicolas barbosa <nico.mb2003@gmail.com>
2023-09-23 14:29:47 -0300

    Atualizações das classes

--- app.py
+++ app.py
@@ -10,14 +10,42 @@ def index():
     return make_response (
         jsonify(
             mensagem='Lista de cadastros.',
-            dados=Cadastro
+            cadastro=Cadastro
         )
     )
 
 @app.route('/cadastro', methods=['POST'])
 def create_cadastro():
     cadastro = request.json
-    return "200ok"
+    Cadastro.append(cadastro)
+    return "Processo Cadastrado com sucesso!!"
 
+
+@app.route('/update', methods=['PUT'])
+def update_cadastro():
+    cadastro = request.get_json()
+    return "Processo Atualizado com sucesso!!"
+
+@app.route('/cadastros/<int:cadastro_id>', methods=['DELETE'])
+def delete_cadastro(cadastro_id):
+    cadastro = next((c for c in cadastros if c['id'] == cadastro_id), None)
+
+    if cadastro is None:
+        return jsonify({"mensagem": "Cadastro não encontrado"}), 404
+
+    cadastros.remove(cadastro)
+    return jsonify({"mensagem": "Cadastro removido com sucesso"})
+
+    cadastro.remove(cadastro)
+    # i = 0
+    # for id in Cadastro:
+    #     if cadastro == Cadastro[i]:
+    #         Cadastro.pop(i)
+    #         return "Processo Excluido com sucesso!!"
+    #     i = i + 1
+    #
+    # return "Item inexistente!!"
+
+@app.route('/')
 
 app.run(port=8000, host='localhost', debug=True)
